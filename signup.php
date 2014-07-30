8e9816461f01b358d01b48f84140b813400e93f0
Zack C-K <clarz016@gmail.com>
2014-07-30 20:17:10 +0100

    Tweaked the signup.php file

--- signup.php
+++ signup.php
@@ -2,14 +2,11 @@
 $email = $_POST["email"];
 $password1 = $_POST["password1"];
 $password2 = $_POST["password2"];
-$number = $_POST["mob_number"];
 if($password1 == $password2){
 $key = "INSERTKEYHERE";
 $encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $password, MCRYPT_MODE_CBC, md5(md5($key))));
 $json = file_get_contents("users.json");
 $json_decoded = json_decode($json, true);
-$privilege_json = file_get_contents("privileges.json");
-$json_decoded2 = json_decode($privilege_json, true);
 if (array_key_exists($usrname,$json_decoded)){
 echo "Sorry that email has already been registered, please try another one";
 }
@@ -18,11 +15,6 @@ $json_decoded[$usrname] = $encrypted;
 $new_json = json_encode($json_decoded);
 $Handle = fopen("users.json", 'w');
 fwrite($Handle, $new_json); 
-fclose($Handle);
-$json_decoded2[$usrname] = "Normal";
-$newjson2 = json_encode($json_decoded2);
-$Handle = fopen("privileges.json", 'w');
-fwrite($Handle, $newjson2); 
 fclose($Handle);
 echo "Sign up successful";
 }
