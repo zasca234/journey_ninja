5b4240a86a63372a561e9294930d4691c6bd6c92
Joseph McCallum-Nattrass <theknightinbinds@gmail.com>
2014-07-31 10:18:34 +0100

    Set up a link to main.js

--- index.php
+++ index.php
@@ -2,49 +2,51 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>JourneyNinja</title>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
-    <!-- Bootstrap core CSS -->
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css" rel="stylesheet">
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>JourneyNinja</title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
+<!-- Bootstrap core CSS -->
+<link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom styles for this template -->
+<link href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css" rel="stylesheet">
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
 
-  <body>
+<body>
 
-    <div class="container">
-      <div class="header">
-        <ul class="nav nav-pills pull-right">
-          <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-        </ul>
-        <h3 class="text-muted">Journey Ninja</h3>
-      </div>
-      <div class="jumbotron">
-        <h1>Journey Ninja</h1>
-        <p class="lead">Unifying travel planning</p>
-        <p><a class="btn btn-lg btn-success" href="signup.html" role="button">Sign up today</a></p>
-      <div class="footer">
-        <p>&copy; Journey Ninja 2014</p>
-      </div>
+<div class="container">
+<div class="header">
+<ul class="nav nav-pills pull-right">
+<li class="active"><a href="#">Home</a></li>
+<li><a href="#">About</a></li>
+</ul>
+<h3 class="text-muted">Journey Ninja</h3>
+</div>
+<div class="jumbotron">
+<h1>Journey Ninja</h1>
+<p class="lead">Unifying travel planning</p>
+<p><a class="btn btn-lg btn-success" href="signup.html" role="button">Sign up today</a></p>
+<div class="footer">
+<p>&copy; Journey Ninja 2014</p>
+</div>
 
-    </div> <!-- /container -->
+</div> <!-- /container -->
 
+<script type="text/javascript" src="main.js"></script>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <!-- Placed at the end of the document so the pages load faster -->
-  </body>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<!-- Placed at the end of the document so the pages load faster -->
+</body>
 </html>
+
