14dd5f17b4bfb82f693d7fe6d961b879fe236b53
Zack C-K <clarz016@gmail.com>
2014-08-02 01:10:05 +0100

    Modified compare.html and main.js

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
 var startPoint;
 var endPoint;
+var mapOptions;
+var markers = new Array();
+var marker_locations = new Array();
+
+var map;
+
+function initialize() {
+	  var centrepoint = new google.maps.LatLng(0.0000000, 0.0000000);
+	var mapOptions = {
+		zoom : 1,
+		center : centrepoint
+	}
+	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+google.maps.event.addListener(map, 'click', addPoint);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function addPoint(event) {
+	clearMarkers();
+    var marker = new google.maps.Marker({
+        position: event.latLng,
+        map: map,
+        draggable: true
+    });
+marker_locations.push(event.latLng);
+console.log(marker_locations);
+markers.push(marker);
+google.maps.event.addListener(marker, 'click', function() {
+        marker.setMap(null);
+        for (var i = 0, I = markers.length; i < I && markers[i] != marker; ++i);
+        markers.splice(i, 1);
+    });
+google.maps.event.addListener(marker, 'dragend', function() {
+ 
+});
+}
+
+//hides the last placed marker
+function clearMarkers() {
+	for (i in markers) {
+		markers[i].setMap(null);
+	}
+}
+
+
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -12,6 +57,7 @@ function getLocation() {
 function showPosition(position) {
     var startPoint = [position.coords.latitude, position.coords.longitude];
     console.log("returned " + startPoint[0] + " " + startPoint[1]);
+	document.getElementById("coords").value = startPoint[0] + "," + startPoint[1];
 }
 
 function showError(error) {
@@ -30,3 +76,4 @@ function showError(error) {
             break;
     }
 }
+
